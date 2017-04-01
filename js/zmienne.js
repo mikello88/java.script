c702a1f3d358bba36e538b9c459c8d05e65bdb38
Michał Dzięcioł <dzieciolmichal@o2.pl>
2017-04-01 09:40:28 +0200

    zmienne

---
+++ js/zmienne.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var imie = "Michał";
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "czerwony";
+//
+//imie = "Sergio";
+//
+//imie = imieDamskie;
+
+console.log(imie);
+console.log(kolor);
+console.log(wiek);
+
+wiek = 21;
+console.log(wiek);
